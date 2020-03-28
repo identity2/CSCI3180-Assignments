4297865e8951a10f9d2ee5a47e5c7137051de045
Yu Chao <casd82@gmail.com>
2020-03-28 20:41:08 +0800

    Added the declaration header for hw3.

--- hw3/task2/perl/Player.pm
+++ hw3/task2/perl/Player.pm
@@ -1,3 +1,19 @@
+# CSCI3180 Principles of Programming Languages
+#
+# --- Declaration ---
+#
+# I declare that the assignment here submitted is original except for source
+# material explicitly acknowledged. I also acknowledge that I am aware of
+# University policy and regulations on honesty in academic work, and of the
+# disciplinary guidelines and procedures applicable to breaches of such policy
+# and regulations, as contained in the website
+# http://www.cuhk.edu.hk/policy/academichonesty/
+#
+# Assignment 3
+# Name : Chao Yu
+# Student ID : 1155053722
+# Email Addr : ychao5@cse.cuhk.edu.hk
+
 use strict;
 use warnings;
 package Player;
